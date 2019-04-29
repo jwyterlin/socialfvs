701de2ff5b037d980bfd2d6b95b3587ab9a04a52
Jhonathan Wyterlin <jhonathanwyterlin@gmail.com>
2019-04-29 01:44:58 -0700

    Responding user with attachment (image) and responding to user's answers

--- index.js
+++ index.js
@@ -79,24 +79,85 @@ app.get('/webhook', (req, res) => {
 });
 
 // Handles messages events
+//function handleMessage(sender_psid, received_message) {
+//
+//  let response;
+//
+//  // Check if the message contains text
+//  if (received_message.text) {    
+//    // Create the payload for a basic text message
+//    response = `You sent the message: "${received_message.text}". Now send me an image!`;
+//  } else if (received_message.attachments) {
+//    // Gets the URL of the message attachment
+//    let attachment_url = received_message.attachments[0].payload.url;
+//  } 
+//  console.log(response); 
+//  
+//  // Sends the response message
+//  callSendAPI(sender_psid, response); 
+//
+//}
 function handleMessage(sender_psid, received_message) {
-
   let response;
-
-  // Check if the message contains text
-  if (received_message.text) {    
-    // Create the payload for a basic text message
-    response = `You sent the message: "${received_message.text}". Now send me an image!`;
-  }
-  console.log(response); 
   
-  // Sends the response message
-  callSendAPI(sender_psid, response); 
-
+  // Checks if the message contains text
+  if (received_message.text) {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
+              }
+            ],
+          }]
+        }
+      }
+    }
+  } 
+  
+  // Send the response message
+  callSendAPI(sender_psid, response);    
 }
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
+
+  let response;
+  
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." }
+  }
+  
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
 
 }
 
@@ -108,7 +169,7 @@ function callSendAPI(sender_psid, response) {
     "recipient": {
       "id": sender_psid
     },
-    "message": {"text": response}
+    "message": response
   };
 
   // Send the HTTP request to the Messenger Platform
