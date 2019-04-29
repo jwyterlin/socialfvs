7e783c4ad4819fedefa1bce7ab8c1e856130ca19
Jhonathan Wyterlin <jhonathanwyterlin@gmail.com>
2019-04-29 12:30:39 -0700

    Removed unnecessary comments

--- index.js
+++ index.js
@@ -79,24 +79,6 @@ app.get('/webhook', (req, res) => {
 });
 
 // Handles messages events
-//function handleMessage(sender_psid, received_message) {
-//
-//  let response;
-//
-//  // Check if the message contains text
-//  if (received_message.text) {    
-//    // Create the payload for a basic text message
-//    response = `You sent the message: "${received_message.text}". Now send me an image!`;
-//  } else if (received_message.attachments) {
-//    // Gets the URL of the message attachment
-//    let attachment_url = received_message.attachments[0].payload.url;
-//  } 
-//  console.log(response); 
-//  
-//  // Sends the response message
-//  callSendAPI(sender_psid, response); 
-//
-//}
 function handleMessage(sender_psid, received_message) {
   let response;
   
