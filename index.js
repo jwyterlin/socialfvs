ac065d8e2a72f00d6da8a9fc3f9cc50ebaeeaf9b
Jhonathan Wyterlin <jhonathanwyterlin@gmail.com>
2019-04-28 23:36:56 -0700

    Added PAGE_ACCESS_TOKEN constant

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Imports dependencies and set up http server
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const
   express = require('express'),
   bodyParser = require('body-parser'),
@@ -39,7 +40,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "facebookf82019socialfvs"
+  let VERIFY_TOKEN = "facebookf82019socialfvs";
     
   // Parse the query params
   let mode = req.query['hub.mode'];
